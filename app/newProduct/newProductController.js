67f232e2f0ad095754f2bb588827e6688e5f8d7d
alexandre.legoff <alexandre.legoff@interalia.net>
2017-10-17 16:43:01 -0500

    datos para objeto de enviar

--- app/newProduct/newProductController.js
+++ app/newProduct/newProductController.js
@@ -3,7 +3,27 @@
 
 app.controller('newProductController',['$scope', 
   function($scope){
-    $scope.title = "Some string";
-    angular.element('id');
+  	var datos = {};
+    $scope.sendProduct = function() {
+    	
+    	datos.range = document.getElementsByClassName("rango")[0].value;
+    	datos.description = document.getElementsByClassName("description")[0].value;
+    	datos.nameOfProduct = document.getElementsByClassName("name")[0].value;
+    	datos.provider = document.getElementsByClassName("provider")[0].value;
+    	datos.sku = document.getElementsByClassName("sku")[0].value;
+    	datos.picture = document.getElementsByClassName("picture")[0].value;
+    	datos.price = document.getElementsByClassName("price")[0].value;
+    	datos.isActive = document.getElementsByClassName("isActive")[0].checked;
+    	datos.id = document.getElementsByClassName("id")[0].value;
+    	datos.tags = [];
+    	for (var i = 0; i < document.getElementsByClassName("tags").length; i++) {
+    		if ( document.getElementsByClassName("tags")[i].checked ) {
+    			datos.tags.push( document.getElementsByClassName("tags")[i].value ); 
+    		}
+    	}
+    	console.log(datos);
+    	
+    };
+    
   }
 ]);
